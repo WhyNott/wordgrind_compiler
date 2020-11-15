19d2ab0e6a0f524b185e1965b1e8db82a02bc80f
Jan Wanot <janwanot@pop-os.localdomain>
2020-11-15 13:48:27 +0000

    Working on deck/actions feature

--- src/action_compiling_idea.js
+++ src/action_compiling_idea.js
@@ -29,14 +29,18 @@
 // }
 
 //todo:
-//next deck field should go to the same one in a lot of cases
+//next deck field should go to the same one in a lot of cases (actually lets worry about that later)
+
 
 
 
 const DB = {
-    store : {},
+    store : {
+        "Player is in {}" : [make_structured_term("Player is in {}", [make_atom("Kitchen")])],
+        "{} is in {}" : [make_structured_term("{} is in {}", [make_atom("Knife"), make_atom("Kitchen")])],
+    },
 
-    function add(fact){
+    add: function (fact){
         const name = fact.is_atom() ? fact.value.toString() : fact.value.functor;
         if (name in this.store){
             this.store[name].push(fact);
@@ -47,7 +51,7 @@ const DB = {
     //note - to remove the need for trailing here, maybe it would make
     //sense to create a 'can_unify' method here, that unifies without making changes
     
-    function remove(fact){
+    remove: function (fact){
         if (name in this.store){
             trail.new_choice_point();
             for (let i = 0; i < this.store[name].length; i++){
@@ -61,17 +65,17 @@ const DB = {
         }
     },
     
-    function match_list(list, index, logic){
+    match_list: function (list, index, logic){
         const item = list[index];
         const name = item.is_atom() ? item.value.toString() : item.value.functor;
         if (name in this.store){
             trail.new_choice_point();
             for (fact of this.store[name]) {
                 if (fact.unify_with(item)){
-                    if (list.length() == (index+1))
+                    if (list.length == (index+1))
                         logic();
                     else
-                        match_one(list, index+1, logc);
+                        this.match_list(list, index+1, logic);
                     trail.restore_choice_point();
                 }
             }
@@ -79,7 +83,7 @@ const DB = {
         } 
         
     },
-    function match(logic, ...facts) {
+    match: function (logic, ...facts) {
         if (facts.length == 0)
             logic();
         else
@@ -101,7 +105,7 @@ const decks = {
                 
                 name.unify_with(make_structured_term("Walk from {} to {}", [var_A, var_B]));
                 description.unify_with(make_structured_term("You walk from {} to {}. You are now in {}.", [var_A, var_B, var_B]));
-                effect.unify_with(make_structured_term("", [
+                effects.unify_with(make_structured_term("", [
                     make_structured_term("Player is in {}", [var_B]),
                     make_structured_term("removes", [
                         make_structured_term("Player is in {}", [var_A]),
@@ -142,7 +146,7 @@ const decks = {
        
                 name.unify_with(make_structured_term("Pick up {}.", [var_Item]));
                 description.unify_with(make_structured_term("You have successfully picked up {}.", [var_Item]));
-                effect.unify_with(make_structured_term("", [
+                effects.unify_with(make_structured_term("", [
                     make_structured_term("Player has {}", [var_Item]),
                     make_structured_term("removes", [
                         make_structured_term("{} is in {}", [var_Item, var_Place]),
@@ -174,7 +178,7 @@ const decks = {
                 
                 name.unify_with(make_structured_term("Drop {}.", [var_Item]));
                 description.unify_with(make_structured_term("You have dropped {}.", [var_Item]));
-                effect.unify_with(make_structured_term("", [
+                effects.unify_with(make_structured_term("", [
                     make_structured_term("{} is in {}", [var_Item, var_Place]),
                     make_structured_term("removes", [
                         make_structured_term("Player has {}", [var_Item]),
@@ -193,8 +197,8 @@ const decks = {
                 name.unify_with(make_atom("Use computer."));
                 next_deck.unify_with(make_atom("Computer"));
                 DB.match(cont_0,
-                    make_structured_term("Player is in {}", [make_atom("Living Room")]);
-                );
+                         make_structured_term("Player is in {}", [make_atom("Living Room")])
+                        );
             },
 
         ],
@@ -301,7 +305,7 @@ function find_choices(){
         let found_choice = {};
         found_choice.name = name.pprint(bracketed = false);
         found_choice.description = description.pprint(bracketed = false);
-        found_choice.effects = effects.bound() ? effects.value.args : null;
+        found_choice.effects = effects.bound() ? effects.dereferenced_value().value.args : null;
         found_choice.next_deck = next_deck.bound() ? next_deck.value.value : null;
         choices.push(found_choice);
     };
@@ -320,7 +324,7 @@ function find_choices(){
 
 function choose_action(late){
     const deck = decks[current_deck.deck];
-    const actions = late ? deck.early_actions : deck.late_actions;
+    const actions = late ? deck.early_actions : deck.late_actions
     
     const name        = make_empty_variable("name"); 
     const description = make_empty_variable("description"); 
@@ -372,22 +376,22 @@ function make_choice(n){
     current_deck.pre_choice = false;
     while(true){
         
-        if state == STATES.LATE {
+        if (state == STATES.LATE) {
         //code for executing late action 
             let action = choose_action(late=true);
-            if action === null {
+            if (action === null) {
                 state = STATES.POP;
             } else {
                 current_element = action;
                 apply_element(current_element);
-                if current_element.next_deck === null {
+                if (current_element.next_deck === null) {
                     state = STATES.LATE;
                 } else {
                     current_deck.pre_choice = false;
                     state = STATES.PUSH;
                 }
             }
-        } else if state == STATES.EARLY {
+        } else if (state == STATES.EARLY) {
             //code for executing early action
             let action = choose_action(late=false);
             if (action === null) {
@@ -404,9 +408,9 @@ function make_choice(n){
                 }
             }
             
-        } else if state == STATES.POP {
+        } else if (state == STATES.POP) {
             //code for popping a deck from stack
-            if (deck_stack.length() > 0)
+            if (deck_stack.length > 0)
                 current_deck = deck_stack.pop();
             else
                 current_deck = {deck: "default", pre_choice:true};
@@ -416,7 +420,7 @@ function make_choice(n){
             else
                 state = STATES.LATE;
             
-        } else if state == STATES.PUSH {
+        } else if (state == STATES.PUSH) {
             //code for pushing current to the stack and seting lead as current deck
             if (current_deck != "default") {
                 deck_stack.push(current_deck);
@@ -429,5 +433,118 @@ function make_choice(n){
         }
 
     }
+
+}
+
+
+const predicates = {
+    //wrote that one manually
+    '{} is not {}':(A, B, index, cont) => {
+        if (!A.unify_with(B)){
+            cont();
+        }
+    },
+    
+    '{} is a room': (head_0, index, cont_0) => {
+        new_backup_frame();
+        dc.add_new_step('<{} is a room> Entry' + (index == 1 ? '' : index));
+
+        trail.new_choice_point();
+
+        {
+            const model = make_atom_model('Kitchen');
+            const model_name = model.pprint();
+            if (head_0.unify_with(model)){
+                dc.add_new_step(`${head_0.direct_name()} = ${model_name}`);
+                cont_0();
+            } else {
+                dc.add_new_step(`Failed: ${head_0.dereferenced_value().direct_name()} = ${model_name}`);
+            }
+        }
+
+        trail.restore_choice_point();
+        dc.add_new_step('backup restored');
+
+        {
+            const model = make_atom_model('Living Room');
+            const model_name = model.pprint();
+            if (head_0.unify_with(model)){
+                dc.add_new_step(`${head_0.direct_name()} = ${model_name}`);
+                cont_0();
+            } else {
+                dc.add_new_step(`Failed: ${head_0.dereferenced_value().direct_name()} = ${model_name}`);
+            }
+        }
+
+        trail.restore_choice_point();
+        dc.add_new_step('backup restored');
+        trail.remove_choice_point();
+
+        remove_backup_frame();
+    },
+
+    '{} is the followup text for {}': (head_0, head_1, index, cont_0) => {
+        new_backup_frame();
+        dc.add_new_step('<{} is the followup text for {}> Entry' + (index == 1 ? '' : index));
+
+        const cont_1 = () => {
+            {
+                const model = make_atom_model('Knife');
+                const model_name = model.pprint();
+                if (head_1.unify_with(model)){
+                    dc.add_new_step(`${head_1.direct_name()} = ${model_name}`);
+                    cont_0();
+                } else {
+                    dc.add_new_step(`Failed: ${head_1.dereferenced_value().direct_name()} = ${model_name}`);
+                }
+            }
+
+        };
+        const cont_2 = () => {
+            {
+                const model = make_atom_model('TV Remote');
+                const model_name = model.pprint();
+                if (head_1.unify_with(model)){
+                    dc.add_new_step(`${head_1.direct_name()} = ${model_name}`);
+                    cont_0();
+                } else {
+                    dc.add_new_step(`Failed: ${head_1.dereferenced_value().direct_name()} = ${model_name}`);
+                }
+            }
+
+        };
+        trail.new_choice_point();
+
+        {
+            const model = make_atom_model('It is really shiny.');
+            const model_name = model.pprint();
+            if (head_0.unify_with(model)){
+                dc.add_new_step(`${head_0.direct_name()} = ${model_name}`);
+                cont_1();
+            } else {
+                dc.add_new_step(`Failed: ${head_0.dereferenced_value().direct_name()} = ${model_name}`);
+            }
+        }
+
+        trail.restore_choice_point();
+        dc.add_new_step('backup restored');
+
+        {
+            const model = make_atom_model('It seems to be missing batteries.');
+            const model_name = model.pprint();
+            if (head_0.unify_with(model)){
+                dc.add_new_step(`${head_0.direct_name()} = ${model_name}`);
+                cont_2();
+            } else {
+                dc.add_new_step(`Failed: ${head_0.dereferenced_value().direct_name()} = ${model_name}`);
+            }
+        }
+
+        trail.restore_choice_point();
+        dc.add_new_step('backup restored');
+        trail.remove_choice_point();
+
+        remove_backup_frame();
+    },
 
 }
